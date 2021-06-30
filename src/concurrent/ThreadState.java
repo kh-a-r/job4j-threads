4851759fda339193ef0d3f4f04bfb9d9cfb728e4
Administrator <kh-a-r@yandex.ru>
2021-06-30 19:41:19 +0500

    1.1. Состояние нити. [#229175 #172369]

---
+++ src/concurrent/ThreadState.java
@@ -0,0 +1,27 @@
+package concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+        Thread first = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                }
+        );
+        Thread second = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                }
+        );
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED ||
+                second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState());
+            System.out.println(second.getState());
+        }
+        System.out.println("Finished");
+    }
+}
+
+
