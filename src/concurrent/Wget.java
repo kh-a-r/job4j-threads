41d346ce3337ab400bde95ec2760bdf09efbc682
Administrator <kh-a-r@yandex.ru>
2021-07-29 00:28:27 +0500

    4. Скачивание файла с ограничением. [#144271]

--- src/concurrent/Wget.java
+++ src/concurrent/Wget.java
@@ -1,21 +1,53 @@
 package concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        for (int i = 0; i <= 100; i++) {
-                            Thread.sleep(300);
-                            System.out.print("\rLoading : " + i + "%");
-                        }
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.time.Duration;
+import java.time.Instant;
+
+public class Wget  implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long start = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                long finish = System.currentTimeMillis();
+                long timeSpent = finish - start;
+                if (1024 / timeSpent < speed) {
+                    Thread.sleep(1024/speed - timeSpent);
                 }
-        );
-        thread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException i) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+        int speed = Integer.parseInt("1");
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
+
 
 
