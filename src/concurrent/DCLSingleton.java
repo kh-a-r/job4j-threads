1f5adff0ba5b56142429ef18ce6734e0a86396b9
Administrator <kh-a-r@yandex.ru>
2021-07-31 20:36:27 +0500

    2. Модель памяти Java [#267917 #183914]

---
+++ src/concurrent/DCLSingleton.java
@@ -0,0 +1,21 @@
+package concurrent;
+
+public class DCLSingleton {
+    private static volatile DCLSingleton inst;
+
+    private DCLSingleton() {
+    }
+
+    public static DCLSingleton instOf() {
+
+        if (inst == null) {
+
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+}
