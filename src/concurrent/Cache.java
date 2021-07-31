7ebe188fdd2d41c3ad4c6133d79d7e89bfc01848
Administrator <kh-a-r@yandex.ru>
2021-07-31 10:24:51 +0500

    1. Синхронизация общих ресурсов. [#1096]

---
+++ src/concurrent/Cache.java
@@ -0,0 +1,12 @@
+package concurrent;
+
+public class Cache {
+    private static Cache cache;
+
+    public synchronized static Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
