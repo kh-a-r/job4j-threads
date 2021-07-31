5b77bb181a0f60674945734a59508f089bb98fe3
Administrator <kh-a-r@yandex.ru>
2021-07-31 20:37:48 +0500

    3. Immutable объекты [#267918 #183980]

--- src/concurrent/Node.java
+++ src/concurrent/Node.java
@@ -4,7 +4,7 @@ public final class Node {
     private final Node<T> next;
     private final T value;
 
-    public NOde(Node<T> next, T value) {
+    public Node(Node<T> next, T value) {
         this.next = next;
         this.value = value;
     }
