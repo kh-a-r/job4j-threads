6e523a3969d34e48f09554fc09a495edfccaf197
Administrator <kh-a-r@yandex.ru>
2021-07-31 21:04:12 +0500

    4. Thread без общих ресурсов [#267919]

---
+++ src/concurrent/UserCache.java
@@ -0,0 +1,25 @@
+package concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        return new CopyOnWriteArrayList<>(users.values());
+    }
+
+}
