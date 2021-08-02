daf6c004384b181d51a838292f3fd6d88fa7c3f3
Administrator <kh-a-r@yandex.ru>
2021-08-02 19:44:37 +0500

    внесены изменения в файл README
    внесены исправления в файл README

--- src/concurrent/Wget.java
+++ src/concurrent/Wget.java
@@ -1,55 +1,21 @@
 package concurrent;
 
-import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URL;
-
-
-public class Wget  implements Runnable {
-    private final String url;
-    private final int speed;
-
-    public Wget(String url, int speed) {
-        this.url = url;
-        this.speed = speed;
-    }
-
-    @Override
-    public void run() {
-        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\Administrator\\Desktop\\test.txt")) {
-            byte[] dataBuffer = new byte[1024];
-            int bytesRead;
-            long start = System.currentTimeMillis();
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-                long finish = System.currentTimeMillis();
-                long timeSpent = finish - start;
-                if (1024 / timeSpent < speed) {
-                    Thread.sleep(1024/speed - timeSpent);
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i <= 100; i++) {
+                            Thread.sleep(300);
+                            System.out.print("\rLoading : " + i + "%");
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-                start = System.currentTimeMillis();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException i) {
-            Thread.currentThread().interrupt();
-        }
-    }
-
-
-    public static void main(String[] args) throws InterruptedException {
-        if(args.length < 2) {
-            System.out.println("Параметры запуска заданы некорректно.");
-        }
-            String url = args[0];
-            int speed = Integer.parseInt(args[1]);
-            Thread wget = new Thread(new Wget(url, speed));
-            wget.start();
-            wget.join();
+        );
+        thread.start();
     }
 }
-
 
 
