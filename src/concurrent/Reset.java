4be4d87ced2abf87b5e8f2389dd37a2ddfaa3ec7
Administrator <kh-a-r@yandex.ru>
2021-08-03 14:13:54 +0500

    add 3 condition

--- src/concurrent/Reset.java
+++ src/concurrent/Reset.java
@@ -3,8 +3,8 @@ package concurrent;
 public class Reset {
     public static void main(String[] args) {
         int i = 2;
-        int sum;
+        int sum = 4;
         sum = i + sum;
-
+        System.out.println("Сумма =  " + sum );
     }
 }
