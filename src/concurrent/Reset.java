fd2cad01d13630dac45873f79c03ef5f8610e60b
Administrator <kh-a-r@yandex.ru>
2021-08-03 14:16:57 +0500

    add calc

---
+++ src/concurrent/Reset.java
@@ -0,0 +1,10 @@
+package concurrent;
+
+public class Reset {
+    public static void main(String[] args) {
+        int i = 2;
+        int sum = 4;
+        sum = i + sum;
+        System.out.println("Сумма =  " + sum);
+    }
+}
